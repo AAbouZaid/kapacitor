4ce632b6b8c99a9aad597857b713e97483fa9900
Michael Desa <mjdesa@gmail.com>
2017-08-23 10:52:06 -0400

    Add ability to load tasks/handlers from dir
    Add check for tickscript defined dbrps
    
    Fix error that allowed you to set dbrps twice
    
    Add template and task dbrp via tickscript
    
    Remove dao test file
    
    Fix issue with updating task without dbrp values
    
    Add new template vars structure
    
    Load tasks and templates from directory
    
    Update define-topic-handler subcommand to 1 arg
    
    Add loadHandlers method to load service
    
    Add tests and prevent certain updates to templates
    
    Update parser to define dbrp as reference.referenc
    
    Fix issue where tasks weren't reloaded
    
    Replace correct topic handler
    
    Listen for SIGHUP to reload tasks/templates/handld
    
    Move dbrps type to client package
    
    Unexport internal functions
    
    Address todo items
    
    Make assorted changes suggested in PR
    
    Add test for dbrp
    
    Add tests for node task type and dbrps
    
    Fix comment
    
    Use ServeHTTP instead of making actual http req
    
    Add soft/hard configuration option
    
    Fix typos and remove TODO comments
    
    Don't error if the directories are missing
    
    Append load service
    
    Move util methods into task_store package
    
    Improve error messaging
    
    Clean up kapacitor and kapacitord mains
    
    Remove old TODO comment
    
    Add error check in doDefine
    
    Add load example directory

--- server/config.go
+++ server/config.go
@@ -27,6 +27,7 @@ import (
 	"github.com/influxdata/kapacitor/services/httppost"
 	"github.com/influxdata/kapacitor/services/influxdb"
 	"github.com/influxdata/kapacitor/services/k8s"
+	"github.com/influxdata/kapacitor/services/load"
 	"github.com/influxdata/kapacitor/services/marathon"
 	"github.com/influxdata/kapacitor/services/mqtt"
 	"github.com/influxdata/kapacitor/services/nerve"
@@ -65,6 +66,7 @@ type Config struct {
 	Replay         replay.Config     `toml:"replay"`
 	Storage        storage.Config    `toml:"storage"`
 	Task           task_store.Config `toml:"task"`
+	Load           load.Config       `toml:"load"`
 	InfluxDB       []influxdb.Config `toml:"influxdb" override:"influxdb,element-key=name"`
 	Logging        diagnostic.Config `toml:"logging"`
 	ConfigOverride config.Config     `toml:"config-override"`
@@ -205,6 +207,9 @@ func (c *Config) Validate() error {
 	}
 	if err := c.Task.Validate(); err != nil {
 		return errors.Wrap(err, "task")
+	}
+	if err := c.Load.Validate(); err != nil {
+		return err
 	}
 	// Validate the set of InfluxDB configs.
 	// All names should be unique.
