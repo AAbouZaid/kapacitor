137bb5ec606eb0e46d554e001dec80cc960d13cd
Michael Desa <mjdesa@gmail.com>
2017-09-18 11:52:34 -0400

    Address issues from PR

--- server/server.go
+++ server/server.go
@@ -894,6 +894,10 @@ func (s *Server) Open() error {
 		return err
 	}
 
+	if err := s.LoadService.Load(); err != nil {
+		return fmt.Errorf("loading tasks/templates/handlers: %s", err)
+	}
+
 	go s.watchServices()
 	go s.watchConfigUpdates()
 
